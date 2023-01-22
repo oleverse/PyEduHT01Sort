4023f2189fb11d9d2ef3dafb8d2ff33055734296
Oleksii Petrashov <oleksii.petrashov@gmail.com>
2023-01-22 23:01:32 +0200

    Path is taken as a CLI parameter or as a user input

--- sort.py
+++ sort.py
@@ -1,7 +1,19 @@
+import sys
 from functions import get_file_list
 
+scan_dir = None
 
-scan_dir = "/tmp/test_dir"
+if len(sys.argv) == 1:
+  print("Можна передати шлях до каталогу у якості праметра командного рядка наступним чином:")
+  print(f"\n\tpython {sys.argv[0]} <шлях_до_каталогу>\n")
+  print("або ввести з клавіатури нижче.\n")
+  try:
+    scan_dir = input("Введіть шлях до каталогу: ")
+  except KeyboardInterrupt:
+    print("")
+    exit()
+else:
+  scan_dir = sys.argv[1]
 
 file_list = get_file_list(scan_dir)
 
